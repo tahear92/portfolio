efaacb1f09e2c90c8f18f39484db72aac0730846
tahear92 <qalliutahir@gmail.com>
2022-12-28 21:55:56 +0300

    fix hamburger menu close on click

--- src/app.js
+++ src/app.js
@@ -1,41 +1,31 @@
-const header = document.querySelector("header");
+const headerElement = document.querySelector("header");
 const menu = document.querySelector('#menu-icon');
 const navbar = document.querySelector('navbar');
+const hamburger_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+const listItems = document.querySelectorAll(".mobile-nav .list li")
 
 window.onload = function () {
+	// Toggle classes in header on scrolling
     window.addEventListener ("scroll", function() {
-        header.classList.toggle ("sticky", this.window.scrollY > 0);
+        headerElement.classList.toggle ("sticky", this.window.scrollY > 0);
 
         if (window.pageYOffset > 100) {
-			document.querySelector("header").classList.add('is-scrolling');
+			headerElement.classList.add('is-scrolling');
 		} else {
-			document.querySelector("header").classList.remove('is-scrolling');
+			headerElement.classList.remove('is-scrolling');
 		}
     });
 
-	// window.addEventListener('scroll', function (e) {
-	// 	if (window.pageYOffset > 100) {
-	// 		document.querySelector("header").classList.add('is-scrolling');
-	// 	} else {
-	// 		document.querySelector("header").classList.remove('is-scrolling');
-	// 	}
-	// });
-
-	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
-
-	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
+	// Toggle classes on hamburger button and mobile menu on button click
+	hamburger_btn.addEventListener('click', function () {
+		hamburger_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
 	})
-    
+
+	// Close mobile menu on item click
+	listItems.forEach(listItem => listItem.addEventListener("click", () => {
+		hamburger_btn.classList.remove("is-active");
+		mobile_menu.classList.remove("is-active");
+	}))
 }
-
-document.querySelectorAll(".mobile-nav").forEach(n => n. addEventListener("click", () => {
-    menu_btn.classList.remove("active");
-    mobile_menu.classList.remove("active");
-}))
-
-
-
-
